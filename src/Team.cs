3a7ae0525d0efaed60071b19d78d18a3a4b38122
STG-Team <stg@mtrummer.de>
2017-02-28 13:36:44 +0100

    neue Files

---
+++ src/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    public class Team
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string instance_id { get; set; }
+        public string commit_id_at_start { get; set; }
+        public int ranking { get; set; }
+        public string version { get; set; }
+        public int gained { get; set; }
+        public string commit_id { get; set; }
+        public int points { get; set; }
+        public double relative_points { get; set; }
+        public bool deployed { get; set; }
+    }
+}
---
+++ src/Tournament.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+
+
+
+
+    public class Tournament
+    {
+        public string id {get; set;}
+        public string tournament_id { get; set; }
+        public DateTime started { get; set; }
+        public Status status { get; set; }
+        public Team[] teams { get; set; }
+        public string salted_response_token { get; set; }
+        public DateTime finished { get; set; }
+    }
+}
